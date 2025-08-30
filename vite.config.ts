aba73b5d0da378914dd77f8c99f5019978b66a0d
dostoyanov <dstoyanov@vedamo.com>
2025-08-30 23:40:21 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/fm-5-countries-api',
+  base: process.env.VERCEL ? '/' : '/fm-5-countries-api/',
 });
